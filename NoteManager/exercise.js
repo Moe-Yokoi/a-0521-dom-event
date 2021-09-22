c0dfc901029148a493565260ec65c37a3188ba6c
Moe <momo_yuzu_77@yahoo.co.jp>
2021-09-21 20:06:00 -0700

    tried assignment

--- NoteManager/exercise.js
+++ NoteManager/exercise.js
@@ -1,1 +1,19 @@
 //solution goes here....
+
+
+const addItemInput = document.querySelector('#add-input')
+const itemUL = document.querySelector('#list')
+const form = document.querySelector('#add')
+
+
+form.addEventListener('submit', function(e){
+    
+        const newItemText = addItemInput.value
+        const newItem = document.createElement('li')
+        newItem.innerText = newItemText
+        itemUL.appendChild(newItem)
+        addItemInput.value = ''
+        
+
+})
+
